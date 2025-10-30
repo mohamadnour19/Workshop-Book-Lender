fd4e9e152db765e6344b01185b43879378897780
Mohamad Nour Shich Khalil <mohmad200.mm16@gmail.com>
2025-10-30 19:28:42 +0100

    Get thinks ready to App.java

--- src/main/java/se/lexicon/model/Book.java
+++ src/main/java/se/lexicon/model/Book.java
@@ -1,5 +1,78 @@
 package se.lexicon.model;
 
 public class Book {
-    // todo: needs completion
+    private String id;
+    private String title;
+    private String author;
+    private boolean available;
+    private Person borrower;
+
+    // Constructor for available book
+    public Book(String title, String author) {
+        this.id = generateId();
+        this.title = title;
+        this.author = author;
+        this.available = true;
+        this.borrower = null;
+    }
+
+    // Constructor for book with borrower
+    public Book(String title, String author, Person borrower) {
+        this.id = generateId();
+        this.title = title;
+        this.author = author;
+        this.available = false;
+        this.borrower = borrower;
+    }
+
+    private String generateId() {
+        return "B" + System.currentTimeMillis();
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public Person getBorrower() {
+        return borrower;
+    }
+
+    // Setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public void setBorrower(Person borrower) {
+        this.borrower = borrower;
+    }
+
+    public String getBookInformation() {
+        String status = available ? "Available" : "Borrowed by: " +
+                (borrower != null ? borrower.getFirstName() + " " + borrower.getLastName() : "Unknown");
+        return "Book ID: " + id +
+                ", Title: " + title +
+                ", Author: " + author +
+                ", Status: " + status;
+    }
 }
--- src/main/java/se/lexicon/model/Person.java
+++ src/main/java/se/lexicon/model/Person.java
@@ -1,5 +1,82 @@
 package se.lexicon.model;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person {
     // todo: needs completion
+
+    private static int  seguence = 0;
+    private int id;
+    private String firstName;
+    private String lastName;
+    private List<Book> borrowedBooks;
+
+
+
+    public Person(String firstName, String lastName) {
+        this.id = this.getNextId();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.borrowedBooks = new ArrayList<>();
+    }
+    private int getNextId() {
+        return ++seguence;
+    }
+    //Setters and Getters
+    public int getId() {
+        return id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void setfirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setlastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void loanBook(Book book) {
+        if (book.isAvailable()) {
+            book.setAvailable(false);
+            book.setBorrower(this);
+            borrowedBooks.add(book);
+            System.out.println(firstName + " borrowed book: " + book.getTitle());
+
+        } else  {
+            System.out.println("Book is already borrowed: " + book.getTitle());
+        }
+
+    }
+    public void returnBook(Book book) {
+        if(borrowedBooks.contains(book)) {
+            book.setAvailable(true);
+            book.setBorrower(null);
+            borrowedBooks.remove(book);
+            System.out.println(firstName + " returned book: " + book.getTitle());
+        }
+        else {
+            System.out.println("This person hasn't borrowed this book: " + book.getTitle());
+        }
+    }
+    public String getPersonInformation() {
+    return "Person ID: " + id +
+            ", Name: " + firstName + " " + lastName + ", Number of borrowed books: " + borrowedBooks.size();
+
+
+    }
+
+
+// Constructors
+
+
 }
